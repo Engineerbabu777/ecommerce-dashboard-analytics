400dc480feb72503ee8e712d1915d7187a96cc94
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-09-16 23:40:26 +0500

    add: added: more files changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,60 @@
-import Image from "next/image";
+import { BarGraph } from "@/components/dashboard/bar-chart";
+import { HorizontalGraph } from "@/components/dashboard/horizontal-bar-chart";
+import { PieGraph } from "@/components/dashboard/pie-chart";
+import { RadarGraph } from "@/components/dashboard/radar-chart";
+import Summary from "@/components/dashboard/summary";
+import { TopCustomers } from "@/components/dashboard/top-customers";
+import { TopProducts } from "@/components/dashboard/top-products";
+import { redirect } from "next/navigation";
+import { auth } from "@/server/auth";
+import { db } from "@/server/db";
+// async function getCustomers(): Promise<Customers[]> {
+//   const res = await fetch(
+//     "https://66a6d52223b29e17a1a39127.mockapi.io/Customers",
+//     { cache: "no-store" }
+//   );
+//   const data = await res.json();
+//   return data;
+// }
 
-export default function Home() {
+export default async function Home() {
+  const customers = await db.customers.findMany({});
+  const products = await db.product.findMany({});
+  const session = await auth();
+  //CHECK IF A USER IS SIGNED IN
+  if (!session) {
+    redirect("/login");
+  }
+
+  const topCustomers = customers
+    .sort((a, b) => b.orders - a.orders)
+    .slice(0, 4);
+
+  const topProducts = products
+    .sort((a, b) => b.revenue - a.revenue)
+    .slice(0, 4);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="p-4 grid gap-5">
+      <Summary />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="grid lg:grid-cols-2 gap-10">
+        <BarGraph />
+        <RadarGraph />
+      </div>
+
+      <div className="grid lg:grid-cols-2 gap-10">
+        <TopProducts
+          data={topProducts.map((product) => ({
+            ...product,
+            image: product.image ?? "",
+          }))}
+        />
+        <PieGraph />
+      </div>
+      <div className="grid lg:grid-cols-2 gap-10">
+        <HorizontalGraph />
+        <TopCustomers data={topCustomers} />
+      </div>
     </div>
   );
 }
